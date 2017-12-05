5f806e4a14c5f5433b6e244aa6229bdd8ca0950b
AshtonStephens <ashton.cs.stephens@gmail.com>
2017-12-05 15:10:45 -0800

    testfiles from lab assignments

---
+++ tests/lab3.asm
@@ -0,0 +1,14 @@
+#
+# lab3.asm
+# 
+#   Test Code EE126 lab 3
+#
+    lw $t2, 0($z)
+L1: beq $t0, $t2, L2
+    sub $s0, $s0, $s1
+    add $t0, $t0, $t1
+    j L1
+L2: or $s2, $s0, $t3
+    and $s2, $s2, $s3
+    sw $s2, 4($t3)
+    nop
---
+++ tests/lab5.asm
@@ -0,0 +1,15 @@
+#
+# lab5.asm
+# 
+#   Test Code from EE126 lab 5
+#
+    lw $t0, 0x0($zero)
+    add $t0, $t0, $t0
+    add $t1, $t0, $t0
+    sub $t2, $t1, $t0
+    sw $t2, 0x4($zero)
+    sw $t2, 0x6($zero)
+    nop // sw -> ID
+    nop // sw -> EX
+    nop // sw -> MEM
+    nop // sw -> WB
