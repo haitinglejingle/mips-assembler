6ada3cbe1992d8c6a8be6942cb6cb60297077d05
AshtonStephens <ashton.cs.stephens@gmail.com>
2017-12-05 15:32:39 -0800

    adding main c++ file

---
+++ mips_assembler.cpp
@@ -0,0 +1,19 @@
+/* 
+ *  mips_assembler.cpp
+ * 
+ *  Purpose: Assembler for MIPS Assembly code
+ *  By: Michelle Chan & Ashton Stephens
+ *  Date: 12/5/17
+ *
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+int main (int argc, char*argv[]) 
+{
+    for (int i = 0; i < argc; fprintf(stdout,"%s\n",argv[i++])); 
+}
